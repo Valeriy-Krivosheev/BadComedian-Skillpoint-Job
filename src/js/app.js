6e316f1b4c175318de5d54e0a8675ed6d5544bb9
Olga <blacksmith@skillpoint.ru>
2022-09-29 18:09:19 +0300

    made a form

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,10 @@
 const swiper = new Swiper('.swiper-support', {
   slidesPerView: 3,
   loop:true,
+  spaceBetween:15,
+  speed: 500,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-
+    nextEl: '.swiper-button-next-support',
+    prevEl: '.swiper-button-prev-support',
   },
 });
