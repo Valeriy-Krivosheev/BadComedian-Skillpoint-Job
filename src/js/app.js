7d9b59e3d88d0259a1aa4a08a053b1f930860609
Valeriy-Krivosheev <80783944+Valeriy-Krivosheev@users.noreply.github.com>
2022-09-30 19:57:39 +0300

    video slider and deleted video

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,43 @@
 const swiper = new Swiper('.swiper-support', {
   slidesPerView: 3,
-  loop:true,
-  spaceBetween:15,
+  loop: true,
+  spaceBetween: 15,
   speed: 500,
   navigation: {
     nextEl: '.swiper-button-next-support',
     prevEl: '.swiper-button-prev-support',
   },
 });
+
+const swiper1 = new Swiper('.swiper-last', {
+  loop: true,
+  spaceBetween: 60,
+  speed: 1000,
+  navigation: {
+    nextEl: '.swiper-button-next-last',
+    prevEl: '.swiper-button-prev-last',
+  },
+  slidesPerView: 1.75,
+  centeredSlider: true,
+  centeredSlides: true,
+  grabCursor: true,
+});
+
+const swiper2 = new Swiper('.swiper-deleted', {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 15,
+  speed: 500,
+  navigation: {
+    nextEl: '.swiper-button-next-deleted',
+    prevEl: '.swiper-button-prev-deleted',
+  },
+});
+
+
+const slides = document.querySelectorAll('.swiper-slide')
+for (slide of slides){
+  slide.addEventListener('click', function (e) {
+    e.target.querySelector('.iframe').src += "?autoplay=1";
+  })
+}
