efd7c9286c0014f757c1b41530002b243109780d
Valeriy-Krivosheev <80783944+Valeriy-Krivosheev@users.noreply.github.com>
2022-10-05 18:31:28 +0300

    adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 const swiper = new Swiper('.swiper-support', {
-  slidesPerView: 3,
+  slidesPerView: 1.25,
   loop: true,
   spaceBetween: 15,
   speed: 500,
@@ -7,24 +7,38 @@ const swiper = new Swiper('.swiper-support', {
     nextEl: '.swiper-button-next-support',
     prevEl: '.swiper-button-prev-support',
   },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+  768: {
+    slidesPerView: 3,
+  },
+}
 });
 
 const swiper1 = new Swiper('.swiper-last', {
   loop: true,
-  spaceBetween: 60,
   speed: 1000,
   navigation: {
     nextEl: '.swiper-button-next-last',
     prevEl: '.swiper-button-prev-last',
   },
-  slidesPerView: 1.75,
+  slidesPerView: 1,
+  spaceBetween: 30,
   centeredSlider: true,
   centeredSlides: true,
   grabCursor: true,
+  breakpoints: {
+    480: {
+      spaceBetween: 60,
+      slidesPerView: 1.75,
+    },
+  }
 });
 
 const swiper2 = new Swiper('.swiper-deleted', {
-  slidesPerView: 3,
+  slidesPerView: 1.25,
   loop: true,
   spaceBetween: 15,
   speed: 500,
@@ -32,10 +46,18 @@ const swiper2 = new Swiper('.swiper-deleted', {
     nextEl: '.swiper-button-next-deleted',
     prevEl: '.swiper-button-prev-deleted',
   },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  }
 });
 
 const swiper3 = new Swiper('.swiper-badtrip', {
-  slidesPerView: 3,
+  slidesPerView: 1.25,
   loop: true,
   spaceBetween: 22,
   speed: 500,
@@ -43,6 +65,14 @@ const swiper3 = new Swiper('.swiper-badtrip', {
     nextEl: '.swiper-button-next-badtrip',
     prevEl: '.swiper-button-prev-badtrip',
   },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  }
 });
 
 const slides = document.querySelectorAll('.swiper-slide')
