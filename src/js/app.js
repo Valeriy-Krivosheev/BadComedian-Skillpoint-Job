3cbdfbb86f1f1fc3ef802fdcf3e20ca0b0c72d33
Valeriy-Krivosheev <80783944+Valeriy-Krivosheev@users.noreply.github.com>
2022-10-07 22:16:56 +0300

    Form

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ const swiper = new Swiper('.swiper-support', {
   loop: true,
   spaceBetween: 15,
   speed: 500,
+  slideToClickedSlide: true,
+
   navigation: {
     nextEl: '.swiper-button-next-support',
     prevEl: '.swiper-button-prev-support',
@@ -11,10 +13,10 @@ const swiper = new Swiper('.swiper-support', {
     480: {
       slidesPerView: 2,
     },
-  768: {
-    slidesPerView: 3,
-  },
-}
+    768: {
+      slidesPerView: 3,
+    },
+  }
 });
 
 const swiper1 = new Swiper('.swiper-last', {
@@ -75,9 +77,48 @@ const swiper3 = new Swiper('.swiper-badtrip', {
   }
 });
 
-const slides = document.querySelectorAll('.swiper-slide')
-for (slide of slides){
-  slide.addEventListener('click', function (e) {
-    e.target.querySelector('.iframe').src += "?autoplay=1";
+
+const select = document.getElementById("select")
+const slider = document.getElementById('slider')
+const selected = slider.querySelectorAll('.swiper-slide')
+
+for (let box of selected) {
+  box.addEventListener('click', function () {
+    if (this.classList.contains('selected')) {
+      this.classList.remove('selected');
+    }
+    else {
+      for (let slide of selected) {
+        slide.classList.remove('selected');
+        this.classList.add('selected');
+      }
+    }
+    let currentSlide = slider.querySelector('.selected')
+    let text = currentSlide.querySelector('.support__name').innerHTML.trim()
+    for (let option of select.options) {
+      option.removeAttribute("selected")
+    }
+    select.options[currentSlide.id].selected = 'selected'
+    let newText = select.options[currentSlide.id].textContent
+    newText = text
   })
 }
+
+
+select.addEventListener("change", function () {
+  let selectVal = this.value;
+  swiper.slideToLoop(selectVal, 500, true)
+  for (item of selected) {
+    item.classList.remove('selected');
+    if (item.classList.contains('swiper-slide-active')) {
+      item.classList.add('selected')
+    }
+  }
+});
+
+
+
+
+
+
+
